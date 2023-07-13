83aa05a09f810d05602a5066dfaef597f01669e0
Narazaka <info@narazaka.net>
2023-07-14 00:29:46 +0900

    doc

--- src/util/timestampString.ts
+++ src/util/timestampString.ts
@@ -1,3 +1,8 @@
+/**
+ * "12:03"
+ *
+ * @param date date
+ */
 export function timestampString(date = new Date()) {
   return `${date.getHours().toString().padStart(2, "0")}:${date
     .getMinutes()
