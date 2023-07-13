b5f2e16ae2005f5865e4a83e430a53b12a1a0382
Narazaka <info@narazaka.net>
2023-07-13 22:34:25 +0900

    typedoc

---
+++ typedoc.config.cjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+const fs = require("fs");
+const path = require("path");
+
+/**
+ *
+ * @param {string} root
+ * @returns
+ */
+function listFiles(root) {
+  return fs
+    .readdirSync(root)
+    .map((entry) => path.join(root, entry))
+    .filter((entry) => fs.statSync(entry).isFile());
+}
+
+/** @type {import('typedoc').TypeDocOptions} */
+module.exports = {
+  entryPoints: [...listFiles("src"), ...listFiles("src/util")],
+  basePath: "src",
+  out: "doc",
+};
